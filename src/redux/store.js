98dfb7c9acf42eb45100f7002c95b8ff953dd94e
Ali <abdulmuqsit10101@gmail.com>
2020-05-19 16:15:56 +0500

    redux-logger added to middleware added.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-const middleware = applyMiddleware(thunk);
+const middleware = applyMiddleware(thunk, logger);
 const store = createStore(rootReducer, middleware);
 
 export default store;
